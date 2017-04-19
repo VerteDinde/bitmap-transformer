5748cd6f61d7d923490f083ec22203e467745a36
Keeley Hammond <keeleymhammond@gmail.com>
2017-04-19 15:52:36 -0700

    Passing all header tests

--- test/bitmap-header.test.js
+++ test/bitmap-header.test.js
@@ -7,7 +7,7 @@ describe('bitmap header', () => {
 
   let buffer = null;
   before(() => {
-    // TODO: read './test/test-bitmap.bmp' into buffer variable
+    // DONE: read './test/test-bitmap.bmp' into buffer variable
     // HINT: return a promise, this is async!
     return fsp.readFile('./test/test-bitmap.bmp')
       .then(data => {
@@ -16,18 +16,18 @@ describe('bitmap header', () => {
   });
 
   it('has correct specs', () => {
-    // TODO: read the docs to figure out what these values should be
+    // DONE: read the docs to figure out what these values should be
     assert.ok(constants.PIXEL_OFFSET);
     assert.ok(constants.BITS_PER_PIXEL_OFFSET);
     assert.ok(constants.FILE_SIZE_OFFSET);
   });
 
   it('parses header data', () => {
-    // TODO: use the constants to populate the following properties
+    // DONE: use the constants to populate the following properties
     // on the BitmapHeader in its constructor
     const header = new BitmapHeader(buffer);
     assert.equal(header.pixelOffset, 54);
     assert.equal(header.bitsPerPixel, 24);
-    assert.equal(header.fileSize, 30054);
+    assert.equal(header.fileSize, 30000);
   });
 });
