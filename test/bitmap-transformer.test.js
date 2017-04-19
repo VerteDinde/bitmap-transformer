f55739e76c53e8dc065d5691363897b140fb0e6d
Keeley Hammond <keeleymhammond@gmail.com>
2017-04-19 13:39:08 -0700

    init commit

--- test/bitmap-transformer.test.js
+++ test/bitmap-transformer.test.js
@@ -4,37 +4,38 @@ const BitmapTransformer = require('../lib/bitmap-transformer');
 const invert = require('../lib/invert-transform');
 
 describe('bitmap file transformer', () => {
-    
-    let buffer = null;
-    before(() => {
-        // TODO: read './test/test-bitmap.bmp' into buffer variable
-        // HINT: return a promise, this is async!
 
-        // TODO: the functionality in this before is same as 
-        // other test, can you remove (extract) the duplication?
-    });
+  let buffer = null;
+  before(() => {
+    // TODO: read './test/test-bitmap.bmp' into buffer variable
+    // HINT: return a promise, this is async!
 
-    // "pinning" test, or "snapshot" test
-    it('test whole transform', () => {
-        // use the BitmapTransformer class, 
-        // passing in the buffer from the file read
-        const bitmap = new BitmapTransformer(buffer);
+    // TODO: the functionality in this before is same as 
+    // other test, can you remove (extract) the duplication?
+  });
 
-        // call .transform(), which will modify the buffer.
-        // in this api, you pass in a transformation function
-        bitmap.transform(invert);
+  // "pinning" test, or "snapshot" test
+  it('test whole transform', () => {
+    // use the BitmapTransformer class, 
+    // passing in the buffer from the file read
+    // HINT: use the Bitmap Header class in the constructor
+    const bitmap = new BitmapTransformer(buffer);
 
-        // after above step, the buffer has been modified
-        // and is accessible via bitmap.buffer
+    // call .transform(), which will modify the buffer.
+    // in this api, you pass in a transformation function
+    bitmap.transform(invert);
 
-        // read the output file we saved earlier as
-        // the "standard" expected output file
-        const buffer = fs.readFileSync('./test/inverted-expected.bmp');
-        assert.deepEqual(bitmap.buffer, buffer);
+    // after above step, the buffer has been modified
+    // and is accessible via bitmap.buffer
 
-        // if you don't have a standard file yet, you could write it 
-        // out by commenting above code, using code below and visually inspect
-        // the file for correctness.
-        // return fs.writeFileSync('./test/output.bmp', bitmap.buffer);
-    });
+    // read the output file we saved earlier as
+    // the "standard" expected output file
+    const buffer = fs.readFileSync('./test/inverted-expected.bmp');
+    assert.deepEqual(bitmap.buffer, buffer);
+
+    // if you don't have a standard file yet, you could write it 
+    // out by commenting above code, using code below and visually inspect
+    // the file for correctness.
+    // return fs.writeFileSync('./test/output.bmp', bitmap.buffer);
+  });
 });
