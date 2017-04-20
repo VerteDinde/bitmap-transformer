ba67e9ba4e34136427407b14f4c88ae592b7b4f2
Keeley Hammond <keeleymhammond@gmail.com>
2017-04-19 17:36:02 -0700

    All tests passing

--- test/bitmap-transformer.test.js
+++ test/bitmap-transformer.test.js
@@ -1,13 +1,14 @@
 const assert = require('assert');
 const fs = require('fs');
+const fsp = require('fs-promise');
 const BitmapTransformer = require('../lib/bitmap-transformer');
-const invert = require('../lib/invert-transform');
+const invert = require('../lib/invert-transformer');
 
 describe('bitmap file transformer', () => {
 
   let buffer = null;
   before(() => {
-    // TODO: read './test/test-bitmap.bmp' into buffer variable
+    // DONE: read './test/test-bitmap.bmp' into buffer variable
     // HINT: return a promise, this is async!
     return fsp.readFile('./test/test-bitmap.bmp')
       .then(data => {
@@ -33,8 +34,8 @@ describe('bitmap file transformer', () => {
 
     // read the output file we saved earlier as
     // the "standard" expected output file
-    const buffer = fs.readFileSync('./test/inverted-expected.bmp');
-    assert.deepEqual(bitmap.buffer, buffer);
+    const expectedBuffer = fs.readFileSync('./test/inverted-expected.bmp');
+    assert.deepEqual(bitmap.buffer, expectedBuffer);
 
     // if you don't have a standard file yet, you could write it 
     // out by commenting above code, using code below and visually inspect
